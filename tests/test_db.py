b3115cb202503717e7e37d65b3993d2849624622
Yevgeniy <cool.zhekar@yandex.ru>
2024-11-26 19:02:00 +0300

    create tests

---
+++ tests/test_db.py
@@ -0,0 +1,16 @@
+import pytest
+
+from tasks_manager.tasks_manager_greenatom.general_code.db import DataBase
+
+@pytest.fixture()
+def task():
+    d_b = DataBase()
+    d_b.add_task('A', 'b', 'c', '1', '2')
+    return d_b.get_all()
+
+def test_add_task(task):
+    assert task == [(1, 'A', 'b', 'c', 1, '2')]
+
+
+if __name__=='__main__':
+    pytest.main()
